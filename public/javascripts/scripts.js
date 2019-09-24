0ab40643f7a3482a730442ed8ce5a76aa40b3e13
Your Name <you@example.com>
2019-09-24 15:54:23 -0700

    styling done

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,7 @@
 function findBook(){
     var userSearch = document.getElementById('userInput').value;
     var bookResult = document.getElementById('result');
+    // document.getElementById('main-body').css.style.display="none";
 
     bookResult.innerHTML = "";
 
@@ -11,6 +12,8 @@ function findBook(){
         success: function(book){
             console.log(book);
             for(var i = 0; book.items.length; i++){
+          
+
                 var wrapperDiv = document.createElement('div');
                 wrapperDiv.className = 'media';
                 // create img element for images
@@ -30,16 +33,38 @@ function findBook(){
                 wrapperDiv.appendChild(div);
                 // create h5 element for author
                 var author = document.createElement('h6');
-                author.innerHTML = book.items[i].volumeInfo.authors;
+                author.innerHTML ='<b>Author:</b>' + ' ' + book.items[i].volumeInfo.authors;
                 div.appendChild(author);
                 // create paragraph for country
                 var country = document.createElement('p');
-                country.innerHTML = 'Country:' + ' ' + book.items[i].accessInfo.country;
+                country.innerHTML = '<b>Country:</b>' + ' ' + book.items[i].accessInfo.country;
                 div.appendChild(country);
+                 //create paragraph for page Count
+
+                 var pagecount=document.createElement('p');
+                 pagecount.innerHTML ='<b>Pages:</b>'+' '+book.items[i].volumeInfo.pageCount;
+                 div.appendChild(pagecount);
+
+               //create element for date
+               var pubyear=document.createElement('p');
+                pubyear.innerHTML='<b>Published:</b>'+' '+book.items[i].volumeInfo.publishedDate;
+                div.appendChild(pubyear);
+                
+                //Create element for Publisher
+                var publisher = document.createElement('p');
+                publisher.innerHTML ='<b>Publisher:</b>'+' '+book.items[i].volumeInfo.publisher;
+                div.appendChild(publisher);
+
                 // create element for discription
                 var desc = document.createElement('p');
                 desc.innerHTML = book.items[i].volumeInfo.description;
                 div.appendChild(desc);
+                //create a tag and 
+                var link=document.createElement('a');
+                link.innerHTML = 'View More';
+                link.href =book.items[i].volumeInfo.previewLink;
+                div.appendChild(link);
+               
                 // create hr to separate every books info
                 var line = document.createElement('hr');
                 // Make every elements as children element of bookResult
